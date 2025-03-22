acf8154af17eb4d05cb0f58f85a71defe0dad5f4
ryuchenn <ryuchenforwork@gmail.com>
2025-03-22 03:37:13 -0400

    no message

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -24,8 +24,8 @@ app.get("/", (req, res) => {
   });
 });
 
-// app.listen(port, () => {
-//     console.log(`Server is running on ${port}`);
-// });
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
 
 export default app;
--- backend/src/api/events/MockData.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { mockEvents } from "../../constants/eventsMock.mock.ts";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json(mockEvents);
-}
