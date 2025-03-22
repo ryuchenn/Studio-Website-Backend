7cb5287970301c92255e84d5ff66ca2a68905763
ryuchenn <ryuchenforwork@gmail.com>
2025-03-22 03:24:29 -0400

    no message

--- backend/src/api/events/index.ts
+++ backend/src/api/events/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json("Event API");
+  res.status(200).json("Event API"); 
 }
