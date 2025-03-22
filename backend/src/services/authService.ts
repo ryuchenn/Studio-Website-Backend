796cda534330d6fc37f9131b4b1db1e783bffe08
ryuchenn <ryuchenforwork@gmail.com>
2025-03-22 03:13:58 -0400

    no message

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,5 +1,5 @@
-import { authDb } from '../DB/index.ts';
+// import { authDb } from '../DB/index.ts';
 
-export async function createUserProfile(uid: string, data: any) {
-  return authDb.collection('users').doc(uid).set(data);
-}
+// export async function createUserProfile(uid: string, data: any) {
+//   return authDb.collection('users').doc(uid).set(data);
+// }
